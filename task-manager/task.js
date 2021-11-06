c2e223cc545da2d211166990d4f1b12e6d543734
Lucaz Lindgren <74791128+LucazLindgren@users.noreply.github.com>
2021-11-06 15:19:22 +0100

    de

--- task-manager/task.js
+++ task-manager/task.js
@@ -1,78 +1,107 @@
-const States = Object.freeze({
-    UNFINISHED: 0,
-    FINISHED: 1,
-});
-
-function Task (name) {
-    this.name = name;
-    this.state = States.UNFINISHED;
-}
-
+let taskList = [];
 let taskHandler = {
-    taskList: [],
     init: function () {
         this.cacheDom();
-        this.bindEvents();
+        this.setEvents();
     },
-    cacheDom: function () {
+    cacheDom: function() {
+        // CACHE HTML ELEMENTS
         this.getInputTask = document.querySelector("#input-task");
         this.getAddBtn = document.querySelector("#add-task-button");
-        this.getTaskList = document.querySelector("#tasks");
+        this.getErrorMsg = document.querySelector(".errorMessage");
+        this.getTasks = document.querySelector("#tasks");
+        this.getTasksChildren = this.getTasks.children;
     },
-    bindEvents: function () {
-        this.getAddBtn.onclick = this.addTask.bind(this);
-    },
-    addTask: function () {
-        let name = this.getInputTask.value;
-        this.taskList.push(new Task(name));
-        this.getInputTask.value = "";
-        renderNewTask();
-        this.evaluateTaskList(); 
+    setEvents: function() {
+        this.getAddBtn.addEventListener("click", hej.bind(this));
+        window.addEventListener("keypress", event => event.code == "Enter" ? hej() : false);       
 
-        function renderNewTask () {
-            let newTaskLi, newTaskCheckbox, newTaskName, newTaskDeleteBtn;
-            // BUILD HTML
-            newTaskLi = document.createElement("li");
-            newTaskLi.setAttribute("class", "task");
-            // CHECKBOX
-            newTaskCheckbox = document.createElement("input");
-            newTaskCheckbox.setAttribute("type", "checkbox");
-            // DELETE BUTTON
-            newTaskDeleteBtn = document.createElement("button");
-            // USER TASK
-            newTaskName = document.createTextNode(name);
-            // APPEND ELEMENTS TO TASKLI
-            newTaskLi.appendChild(newTaskCheckbox);
-            newTaskLi.appendChild(newTaskName);
-            newTaskLi.appendChild(newTaskDeleteBtn);
-            //ADD TASK TI TASK LIST
-            taskHandler.getTaskList.appendChild(newTaskLi);
+        function hej() {
+            taskHandler.resetError(); 
+            let text = taskHandler.getInputTask.value;
+            let result = taskHandler.validateTasktext(text);
+            if(result.valid == true) {
+                taskHandler.addTask(text);
+                taskHandler.getInputTask.value = "";
+            } else taskHandler.error(result.msg);
         }
     },
-    removeTask: function (i) {
-        this.getTaskList.children[i].remove();
-        this.evaluateTaskList();
+    validateTasktext: function (text) {
+        // CONTROL TASK TEXT
+        // RETURNS AN OBJECT
+        if(text == "") return {valid: false, msg: "Enter a task"};
+        if(taskList.some(obj => obj.text == text)) 
+        return {valid: false, msg: "Task already exists"};
+        return {valid: true};
+    },
+    error: function(msg) {
+        this.getInputTask.setAttribute("class", "error");
+        this.getErrorMsg.innerHTML = msg;
+    },
+    resetError: function() {
+        this.getInputTask.removeAttribute("class", "error");
+        this.getErrorMsg.innerHTML = "";
+
+    },
+    addTask: function(text) {
+        const task = {
+            text,
+            checked: false,
+            id: Date.now(),
+        };
+        taskList.push(task);
+        this.renderTask(task);
+        this.evaluateTaskList(); 
+        console.log(taskList);
+    },
+    renderTask: function(task) {
+        let newTaskLi, newTaskCheckbox, newTaskName, newTaskDeleteBtn;
+        // BUILD HTML
+        newTaskLi = document.createElement("li");
+        newTaskLi.setAttribute("class", "task");
+
+        if(task.delelted) {
+            
+        }
+        // CHECKBOX
+        newTaskCheckbox = document.createElement("input");
+        newTaskCheckbox.setAttribute("type", "checkbox");
+        // DELETE BUTTON
+        newTaskDeleteBtn = document.createElement("button");
+        // USER TASK
+        newTaskName = document.createTextNode(task.text);
+        // APPEND ELEMENTS TO TASKLI
+        newTaskLi.appendChild(newTaskCheckbox);
+        newTaskLi.appendChild(newTaskName);
+        newTaskLi.appendChild(newTaskDeleteBtn);
+        //ADD TASK TO TASK LIST
+        taskHandler.getTasks.appendChild(newTaskLi);
+    },
+    deleteTask: function(key) {
+        const index = taskList.findIndex(item => item.id === Number(key));
+        const task = {
+            delelted: true,
+            ...taskList[index]
+        }
+        taskList.splice(index, 1);
+        this.renderTask(task);
+        console.log(taskList);
     },
     changeChecked: function (task) {
-
+        
     },
-    evaluateTaskList: function () {
+    evaluateTaskList: function() {
         let checkbox, deleteBtn;
         // BIND CLICK EVENTS TO ELEMENTS
-        for (let i = 0; i < this.getTaskList.children.length; i++) {
+        for (let i = 0; i < this.getTasksChildren.length ; i++) {
             // ADD CLICK EVENT TO CHECKBOXES
-            checkbox = this.getTaskList.children[i].getElementsByTagName("input")[0];
-            checkbox.onclick = this.changeChecked.bind(this);
+            checkbox = this.getTasksChildren[i].getElementsByTagName("input")[0];
+            checkbox.onclick = this.changeChecked.bind(this, );
             // ADD CLICK EVENT TO DELETE BUTTON
-            deleteBtn = this.getTaskList.children[i].getElementsByTagName("button")[0];
-            deleteBtn.onclick = this.removeTask.bind(this, i);
+            deleteBtn = this.getTasksChildren[i].getElementsByTagName("button")[0];
+            deleteBtn.onclick = this.deleteTask.bind(this, i);
         }
-    },
-    renderAllTasks: function () {
-
     }
-
-    
 }
 
 
