23e79303fa20c79d71131bda916eecca8ff70b88
Lucaz Lindgren <74791128+LucazLindgren@users.noreply.github.com>
2021-12-17 19:01:50 +0100

    w

--- task-manager/index.js
+++ task-manager/index.js
@@ -2,16 +2,22 @@ window.onload = function() {
     taskHandler.init();
 }
 
-// UNDERLINE WHEN FOCUSING ON INPUT TEXT
+// UNDERLINE TAKER INPUT WHEN FOCUSING ON INPUT TEXT OR ADD TASK BTN
 let getTaskerInput = document.querySelector("#tasker-input");
+let getInputBtn = document.querySelector("#add-task-btn");
 let getInputText = document.querySelector("#input-text");
-getInputText.addEventListener("focusin", () => {
+getInputText.addEventListener("focusin", focusIn);
+getInputText.addEventListener("focusout", focusOut);
+getInputBtn.addEventListener("focusin", focusIn);
+getInputBtn.addEventListener("focusout", focusOut);
+
+function focusIn() {
     getTaskerInput.classList.add("focus");
-});
-getInputText.addEventListener("focusout", () => {
+}
+
+function focusOut() {
     getTaskerInput.classList.remove("focus");
-});
 
-
+}
 
 
